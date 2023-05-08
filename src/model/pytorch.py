9c5e6e3ae8a7843d888e950fade1d88f80690950
Zack <zbostock56@gmail.com>
2023-05-08 17:37:26 -0400

    templated data collection and visualization pytorch

--- src/model/pytorch.py
+++ src/model/pytorch.py
@@ -1,10 +1,12 @@
 import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torch import nn # neural networks
+from torch import optim # optimizers
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+# Check PyTorch Version
+print(f"PyTorch Version: {torch.__version__}")
 
 device = (
     "cuda"
@@ -15,42 +17,67 @@ device = (
 )
 print(f"Using {device} device")
 
-# Define model
+# Create Data
+weight = 0.7
+bias = 0.3
+start = 0
+end = 1
+step = 0.0001
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+def plot_predictions(train_data=X_train,
+                     train_labels=y_train,
+                     test_data=X_test,
+                     test_labels=y_test,
+                     predictions=None):
+  plt.figure(figsize = (10, 7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c = "b", s = 4, label = "Training data")
+
+  # Plot testing data in green
+  plt.scatter(test_data, test_labels, c = "g", s = 4, label= "Testing data")
+
+  if predictions is not None:
+    # Plot the predictions
+    plt.scatter(test_data, predictions, c="r", s = 4, label = "Predictions")
+
+  plt.legend(prop = {"size": 14})
+  plt.show()
+
 class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-#            nn.Linear(28*28, 512),
-#            nn.ReLU(),
-#            nn.Linear(512, 512),
-#            nn.ReLU(),
-#            nn.Linear(512, 10)
-        )
+  # Constuctor
+  def __init__(self):
+    super().__init__()
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+    # Model Parameters
+    # requires_grad = true =====> use gradient descent
+    self.weights = nn.Parameter(torch.randn(1,
+                                            requires_grad = True,
+                                            dtype = torch.float32))
+    self.bias = nn.Parameter(torch.randn(1,
+                                         requires_grad = True,
+                                         dtype = torch.float32))
 
-model = NeuralNetwork().to(device)
-print(model)
+    # Define the computation in the model
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    return self.weights * x + self.bias
 
-batch_size = 64
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+# Create a manual random seed
+torch.manual_seed(42)
 
-# Save Model
-#torch.save(model.state_dict(), "model.pth")
-#print("Saved PyTorch Model State to model.pth")
+# Instance of the model
+model = NeuralNetwork()
 
-# Load Model
-#model = NeuralNetwork().to(device)
-#model.load_state_dict(torch.load("model.pth"))
+# Make predictions with model
+with torch.inference_mode():
+  y_preds = model(X_test)
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
+print(y_preds)
+
+plot_predictions(predictions=y_preds);
