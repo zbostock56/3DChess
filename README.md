14291cf73f1b38b1ed54cb87f0b69f548f4421f3
Zack <zbostock56@gmail.com>
2023-05-16 14:48:51 -0400

    Additional changes to readme/preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # 3DChess
+![Title](images/logo.png)
+
+## What is it?
+3DChess is a version of the standard chess game which incorporates more than one board of play. Instead of the standard horizontal, verticle, and diagonal movements, 3DChess incorporates up and down movements between three stacked 8x8 chess boards, allowing for much more complex strategy.
+
+* * *
+## Build Requirements:
+Linux (Debian-like/Ubuntu):
+* libglfw3-dev
+* libclgm-dev 
+
+```bash
+sudo apt-get install -y libglfw3-dev libcglm-dev
+```  
+
+To build:  
+```bash
+make
+```
+
+MacOS (Apple Silicon)
+* Unsupported (OpenGL deprecation)
+* _Note: Build will compile, but graphics will crash_
+
+MacOS (x86-64)
+* Untested
+
+Windows (x86-64)
+* Untested
+
+Windows (ARM)
+* Untested
+* * *
+## Python | Pytorch | Pygame
+
+### Versions
+All Python code was written with `Python 3.6.8` and above. Pytorch version `2.0.0` or higher is *required*. Pygame version ` 2.4.0 ` was used for preprocessing data. Older versions of the above were not tested or will bork if specified.
+
+To check versions
+```python
+import torch
+import sys
+import pygame
+# Python Version
+print(sys.version)
+# Pytorch Version
+print(torch.__version__)
+# Pygame Version
+print(pygame.version.ver)
+```
+
+
+
+* * *
+## Upcoming Features
+* [X] Graphics Support
+* [ ] Neural-netork-based opponent
+* [ ] Testing on Windows (x86-64)
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -4,47 +4,44 @@ import pygame
 pygame.init()
 
 # Set up display
-width, height = 640, 640
+width, height = 1200, 1200
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Data Preprocessing")
 
 # Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+BLACK = (156, 84, 84)
+WHITE = (255, 216, 216)
 
 # Set chess board dimensions
 board_size = 8
-tile_size = height // (board_size * 3)
+tile_size = height // (board_size * 3.5)
+
+# Calculate the new size based on 90% of tile size
+new_size = int(tile_size * 1)
 
 # Load chess piece images
 pieces = {
-  1: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_bishop.png"),
-  2: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_rook.png"),
-  3: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_queen.png"),
-  4: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_pawn.png"),
-  5: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_knight.png"),
-  6: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/w_king.png"),
-  7: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_bishop.png"),
-  8: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_rook.png"),
-  9: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_queen.png"),
-  10: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_pawn.png"),
-  11: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_knight.png"),
-  12: pygame.image.load("/home/zbostock/3DChess/preprocessing/sprites/b_king.png"),
-  0: None, # Empty square
+    1: pygame.transform.scale(pygame.image.load("sprites/w_bishop.png"), (new_size, new_size)),
+    2: pygame.transform.scale(pygame.image.load("sprites/w_rook.png"), (new_size, new_size)),
+    3: pygame.transform.scale(pygame.image.load("sprites/w_queen.png"), (new_size, new_size)),
+    4: pygame.transform.scale(pygame.image.load("sprites/w_pawn.png"), (new_size, new_size)),
+    5: pygame.transform.scale(pygame.image.load("sprites/w_knight.png"), (new_size, new_size)),
+    6: pygame.transform.scale(pygame.image.load("sprites/w_king.png"), (new_size, new_size)),
+    7: pygame.transform.scale(pygame.image.load("sprites/b_bishop.png"), (new_size, new_size)),
+    8: pygame.transform.scale(pygame.image.load("sprites/b_rook.png"), (new_size, new_size)),
+    9: pygame.transform.scale(pygame.image.load("sprites/b_queen.png"), (new_size, new_size)),
+    10: pygame.transform.scale(pygame.image.load("sprites/b_pawn.png"), (new_size, new_size)),
+    11: pygame.transform.scale(pygame.image.load("sprites/b_knight.png"), (new_size, new_size)),
+    12: pygame.transform.scale(pygame.image.load("sprites/b_king.png"), (new_size, new_size)),
+    0: None,  # Empty square
 }
 
 # Load chess board configurations from file
 def load_board_configs(file_name):
   try:
     with open(file_name, "r") as file:
-      line = file.readline()
-      pieces_list = line.strip().split(",")
-      board_configs = [pieces_list[i:i + board_size] for i in range(0, len(pieces_list), board_size)]
-      # Print board_configs
-      print("Board Configurations:")
-      for config in board_configs:
-        print(config)
-      return [board_configs]
+      lines = file.readlines()
+      return [line.strip().split(",") for line in lines]
   except IOError:
     print(f"Error: Could not open file '{file_name}'")
     return None
@@ -52,15 +49,17 @@ def load_board_configs(file_name):
 # Create sprites for chess pieces
 def create_sprites(board_configs):
   sprites = pygame.sprite.Group()
+  print(f"Board config: {board_configs}")
   for level, board_config in enumerate(board_configs):
-    for row in range(board_size):
-      for col in range(board_size):
+    for row in range(len(board_config)):
+      for col in range(len(board_config[row])):
         piece = int(board_config[row][col].strip())
-        print(f"Piece number: {piece}")
+        print(f"piece: {piece} | row: {row} | col: {col}")
         if piece != 0 and piece in pieces:
           x = col * tile_size
-          #y = (level * board_size * tile_size * 3) + (row * tile_size)
-          y = (level * board_size + row) * tile_size * 3
+          x += 400
+          y = level * (board_size * tile_size) + row * tile_size
+          y += 50
           sprite = pygame.sprite.Sprite()
           sprite.image = pieces[piece]
           sprite.rect = sprite.image.get_rect()
@@ -71,18 +70,14 @@ def create_sprites(board_configs):
 
 # Main game loop
 def game_loop():
-  screen_width, screen_height = 1920, 1080
   # Load board configs
-  board_configs = load_board_configs("/home/zbostock/move_data/chess_poss.csv")
+  board_configs = load_board_configs("/home/zbostock/Projects/3DChess/src/move_data/chess_poss.csv")
   if not board_configs:
     return
 
-  # init pygame
-  screen = pygame.display.set_mode((screen_width, screen_height))
-  pygame.display.set_caption("Data Preprocessing")
-
   # Set up sprites
-  sprites = create_sprites(board_configs)
+  current_board = 0
+  sprites = create_sprites(board_configs[current_board])
 
   clock = pygame.time.Clock()
   running = True
@@ -92,16 +87,29 @@ def game_loop():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+          # Move to the next line in the file
+          current_board = (current_board + 1) % len(board_configs)
+          # Clear the sprites and load the new board config
+          sprites.empty()
+          sprites = create_sprites([board_configs[current_board]])
+
 
     # Clear the screen
-    screen.fill(BLACK)
+    screen.fill((0,0,0))
 
     # Draw chess board
-    for level, board_config in enumerate(board_configs):
+    for level in range(3):
       for row in range(board_size):
         for col in range(board_size):
           x = col * tile_size
+          x += 400
           y = level * (board_size * tile_size) + row * tile_size
+          if level == 1:
+            y += 50
+          elif level == 2:
+            y += 100
           color = WHITE if (row + col) % 2 == 0 else BLACK
           pygame.draw.rect(screen, color, (x, y, tile_size, tile_size))
 
